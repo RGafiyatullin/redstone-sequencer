1abbee4be012e80ff74e0edd3893a646c6104e2c
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-04-24 15:11:11 +0300

    PayloadBuilder<Pool, Client> for RedstonePayloadBuilder<EvmConfig> :: build_empty_payload

--- crates/node-redstone-sequencer/src/payload/builder.rs
+++ crates/node-redstone-sequencer/src/payload/builder.rs
@@ -1,15 +1,28 @@
 use std::sync::Arc;
 
+use reth_basic_payload_builder::WithdrawalsOutcome;
+use reth_node_api::PayloadBuilderAttributes;
+use reth_primitives::constants::BEACON_NONCE;
+use reth_primitives::constants::EMPTY_RECEIPTS;
+use reth_primitives::constants::EMPTY_TRANSACTIONS;
+use reth_primitives::Block;
+use reth_primitives::EMPTY_OMMER_ROOT_HASH;
+use reth_primitives::U256;
+use reth_revm::database::StateProviderDatabase;
+use revm::db::states::bundle_state::BundleRetention;
+use revm::db::State;
 use tracing::trace;
 
 use reth_basic_payload_builder::BuildArguments;
 use reth_basic_payload_builder::BuildOutcome;
 use reth_basic_payload_builder::PayloadBuilder;
+use reth_basic_payload_builder::PayloadConfig;
 use reth_node_api::ConfigureEvm;
 use reth_payload_builder::error::PayloadBuilderError;
 use reth_primitives::ChainSpec;
 use reth_provider::StateProviderFactory;
 use reth_transaction_pool::TransactionPool;
+use tracing::warn;
 
 use super::RedstoneBuiltPayload;
 use super::RedstonePayloadBuilderAttributes;
@@ -61,9 +74,112 @@ where
     }
 
     fn build_empty_payload(
-        _client: &Client,
-        _config: reth_basic_payload_builder::PayloadConfig<Self::Attributes>,
+        client: &Client,
+        config: PayloadConfig<Self::Attributes>,
     ) -> Result<Self::BuiltPayload, PayloadBuilderError> {
-        unimplemented!()
+        let PayloadConfig {
+            initialized_block_env,
+            parent_block,
+            attributes,
+            chain_spec,
+            initialized_cfg,
+            extra_data,
+            ..
+        } = config;
+
+        let state = client.state_by_block_hash(parent_block.hash())
+            .inspect_err(|err| warn!(target: "payload_builder", parent_hash=%parent_block.hash(), %err, "failed to get state for empty payload"))?;
+
+        let mut db = State::builder()
+            .with_database(StateProviderDatabase::new(&state))
+            .with_bundle_update()
+            .build();
+
+        let base_fee: u64 = initialized_block_env.basefee.to();
+        let block_number: u64 = initialized_block_env.number.to();
+        let block_gas_limit: u64 = initialized_block_env
+            .gas_limit
+            .try_into()
+            .unwrap_or(u64::MAX);
+
+        reth_basic_payload_builder::pre_block_beacon_root_contract_call(
+            &mut db,
+            chain_spec.as_ref(),
+            block_number,
+            &initialized_cfg,
+            &initialized_block_env,
+            &attributes
+        ).inspect_err(|err| warn!(target: "payload_builder", parent_hash=%parent_block.hash(), %err, "failed to apply beacon root contract call for empty payload"))?;
+
+        let WithdrawalsOutcome { withdrawals_root, withdrawals } = reth_basic_payload_builder::commit_withdrawals(
+            &mut db, chain_spec.as_ref(),
+            attributes.timestamp(),
+            attributes.withdrawals().clone()
+        ).inspect_err(|err| warn!(target: "payload_builder", parent_hash=%parent_block.hash(), %err, "failed to commit withdrawals for empty payload"))?;
+
+        db.merge_transitions(BundleRetention::PlainState);
+
+        // calculate the state root
+        let bundle_state = db.take_bundle();
+        let state_root = state.state_root(&bundle_state)
+            .inspect_err(|err| warn!(target: "payload_builder", parent_hash=%parent_block.hash(), %err, "failed to calculate state root for empty payload"))?;
+
+        let is_cancun_active_now = chain_spec.is_cancun_active_at_timestamp(attributes.timestamp());
+        let is_cancun_active_at_parent =
+            chain_spec.is_cancun_active_at_timestamp(parent_block.timestamp);
+
+        let (excess_blob_gas, blob_gas_used) =
+            match (is_cancun_active_now, is_cancun_active_at_parent) {
+                (true, true) => Some((
+                    reth_primitives::eip4844::calculate_excess_blob_gas(
+                        parent_block.excess_blob_gas.unwrap_or_default(),
+                        parent_block.blob_gas_used.unwrap_or_default(),
+                    ),
+                    0,
+                )),
+                (true, false) => Some((0, 0)),
+                (false, _probably_false_too) => None,
+            }
+            .unzip();
+
+        let header = reth_primitives::Header {
+            parent_hash: parent_block.hash(),
+            ommers_hash: EMPTY_OMMER_ROOT_HASH,
+            beneficiary: initialized_block_env.coinbase,
+            state_root,
+            transactions_root: EMPTY_TRANSACTIONS,
+            withdrawals_root,
+            receipts_root: EMPTY_RECEIPTS,
+            logs_bloom: Default::default(),
+            timestamp: attributes.timestamp(),
+            mix_hash: attributes.prev_randao(),
+            nonce: BEACON_NONCE,
+            base_fee_per_gas: Some(base_fee),
+            number: parent_block.number + 1,
+            gas_limit: block_gas_limit,
+            difficulty: U256::ZERO,
+            gas_used: 0,
+            extra_data,
+            blob_gas_used,
+            excess_blob_gas,
+            parent_beacon_block_root: attributes.parent_beacon_block_root(),
+        };
+
+        let block = Block {
+            header,
+            body: Default::default(),
+            ommers: Default::default(),
+            withdrawals,
+        }
+        .seal_slow();
+
+        Ok(RedstoneBuiltPayload {
+            id: attributes.payload_id(),
+            block,
+            fees: U256::ZERO,
+            sidecars: Default::default(),
+            chain_spec,
+            attributes,
+        })
     }
 }
